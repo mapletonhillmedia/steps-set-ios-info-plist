2962dec6523f87126f930d12599954f630f871cb
Jaspalsinh Chauhan <jachauhan@gmail.com>
2016-04-30 12:51:26 +0530

    Step script fix. Bitrise test, README updated
    Added an sample Info.plist to use for testing

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# steps-set-ios-bundle-identifier
-Set iOS Info.plist - Bundle Identifier
+# Set iOS Info.plist - Bundle Identifier
+
+Sets the Bundle Identifier to the specified value, in the target Info.plist file for the next build.
+
+
+### How to use this Step
+
+Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
+just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
+and call `bitrise run test`.
+
+*Check the `bitrise.yml` file for required inputs which have to be
+added to your `.bitrise.secrets.yml` file!*
+
+*There is a sample Info.plist file in _tmp directory for your testing*
+
+### Change log
+
+Version 1.0.1
+* Initial commit and first step share to Bitrise.
+
+### Credits
+
+Thanks to Bitrise team for this awesome service / prompt support!
--- step.sh
+++ step.sh
@@ -25,51 +25,23 @@ fi
 echo " (i) Provided Info.plist file path: ${info_plist_file}"
 echo " (i) Provided Bundle Identifier: ${bundle_identifier}"
 
-#INFO_PLIST_FILE=`grep infoPlistFile ${manifest_file} | sed 's/.*versionCode="//;s/".*//'`
-#VERSIONNAME=`grep versionName ${manifest_file} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
-
-#if [ -z "${VERSIONCODE}" ] ; then
-#  echo " [!] Could not find current Version Code!"
-#  exit 1
-#fi
-
-#echo "Version code detected: ${VERSIONCODE}"
-#if [ ! -z "${version_code_offset}" ] ; then
-#  echo " (i) Version code offset: ${version_code_offset}"
-
-#  CONFIG_new_version_code=$((${VERSIONCODE} + ${version_code_offset}))
-
-#  echo " (i) Version code: ${CONFIG_new_version_code}"
-#else
-#  echo " (i) Version code: ${CONFIG_new_version_code}"
-#fi
-
-
-#if [ -z "${VERSIONNAME}" ] ; then
-#  echo " [!] Could not find current Version Name!"
-#  exit 1
-#fi
-#echo "Version name detected: ${VERSIONNAME}"
-
-
 # ---------------------
 # --- Main:
 
 # verbose / debug print commands
-
 set -v
-# ---- Set Build Version Code:
 
-#sed -i.bak "s/android:versionCode="\"${VERSIONCODE}\""/android:versionCode="\"${CONFIG_new_version_code}\""/" ${manifest_file}
+# ---- Set Info.plist Bundle Identifier:
+echo ""
+echo ""
+echo " (i) Replacing Bundle Identifier..."
 
-# ---- Set Build Version Code if it was specified:
-#if ! [ -z "${version_name}" ] ; then
-#  sed -i.bak "s/android:versionName="\"${VERSIONNAME}\""/android:versionName="\"${version_name}\""/" ${manifest_file}
-#fi
+ORIGINAL_BUNDLE_IDENTIFIER="$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" ${info_plist_file})"
+echo " (i) Original Bundle Identifier: $ORIGINAL_BUNDLE_IDENTIFIER"
 
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier ${bundle_identifier}" ${info_plist_file}
 
-# ---- Remove backup:
+REPLACED_BUNDLE_IDENTIFIER="$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" ${info_plist_file})"
+echo " (i) Replaced Bundle Identifier: $REPLACED_BUNDLE_IDENTIFIER"
 
-rm -f ${info_plist_file}.bak
-
-# ==> Build Version changed
+# ==> Bundler Identifier patched in Info.plist file for iOS project
